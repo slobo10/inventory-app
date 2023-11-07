f61a3dc45ad7971350ae336aaec93dd92022c911
slobo10 <97814567+slobo10@users.noreply.github.com>
2023-11-06 17:01:33 -0800

    Create styles.tsx

---
+++ constants/styles.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  listScreenText: {
+    fontSize: 100,
+    fontWeight: "bold",
+  },
+});
+
+export default styles;
--- screens/ListScreen.tsx
+++ screens/ListScreen.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import styles from "../constants/styles";
 
 const ListScreen: React.FC = () => {
   return (
     <View>
-      <Text>In the clear!!</Text>
+      <Text style={styles.listScreenText}>In the clear!!</Text>
     </View>
   );
 };
