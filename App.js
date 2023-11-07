9a9bed9944eac5101d60853a0896d60ebf9b5417
slobo10 <97814567+slobo10@users.noreply.github.com>
2023-11-06 16:44:30 -0800

    Environment setup

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>App not ready yet!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 100,
+    fontWeight: "bold",
+  },
+});
